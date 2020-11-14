30a1db8a966a8a46320d488ab1dd750aa2eaa2e1
Michael Ritsch <55855825+michael-ritsch@users.noreply.github.com>
2020-11-15 00:15:19 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,7 @@
 <?php
 
-/**
- * Enqueue styles
- */
-function child_enqueue_styles() {
-
-	wp_enqueue_style( 'vanlife-austria-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), null, 'all' );
-
-}
-
+// Enqueue style
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+function child_enqueue_styles() {
+	wp_enqueue_style( 'mikecodesastrachild-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), null, 'all' );
+}
